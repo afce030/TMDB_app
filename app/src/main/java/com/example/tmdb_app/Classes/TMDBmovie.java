fbc5d23e394e3e1e88b54bbc20c712319625e633
afce030 <andrescardona056@hotmail.com>
2019-07-13 16:42:23 -0500

    creación TMDBclass

---
+++ app/src/main/java/com/example/tmdb_app/Classes/TMDBmovie.java
@@ -0,0 +1,20 @@
+package com.example.tmdb_app.Classes;
+
+public class TMDBmovie {
+
+    int vote_count;
+    int id;
+    boolean video;
+    float vote_average;
+    String title;
+    float popularity;
+    String poster_path;
+    String original_language;
+    String original_title;
+    int[] genre_ids;
+    String backdrop_path;
+    boolean adult;
+    String overview;
+    String release_data;
+
+}
